913e9c134bff8009ba86fe51932810d684e2acb5
lwz <lwz@163.com>
2020-06-13 10:50:12 +0800

    初始化提交

---
+++ src/main/java/top/lwzp/test/HelloSpringBoot.java
@@ -0,0 +1,21 @@
+package top.lwzp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+
+@SpringBootApplication
+@RestController
+public class HelloSpringBoot {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "helloworld";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBoot.class,args);
+    }
+}
