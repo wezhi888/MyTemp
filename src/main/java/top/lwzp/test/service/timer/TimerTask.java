58f6673f5a925a86b3d24669843b09c341b741ce
liuwenzhi <pwd@1nortH>
2020-06-18 10:29:25 +0800

    各个类型的定时任务示例

--- src/main/java/top/lwzp/test/service/timer/TimerTask.java
+++ src/main/java/top/lwzp/test/service/timer/TimerTask.java
@@ -11,10 +11,14 @@ public class TimerTask {
     }
     @Scheduled(fixedDelay = 36000000)
     private void taskFixedDelay(){
-        System.out.println("定时任务执行中");
+        System.out.println("上一次执行完成之后，间隔若干时间再次执行！");
     }
     @Scheduled(fixedRate = 36000000)
     private void taskFixedRate(){
-        System.out.println("定时任务");
+        System.out.println("上一次执行开始之后，间隔若干时间再次执行！");
+    }
+    @Scheduled(initialDelay = 10000,fixedDelay = 36000000)
+    private void taskDelayStart(){
+        System.out.println("第一次在启动之后若干时间之后执行，然后在每次执行完成之后间隔若干时间再次执行！");
     }
 }
