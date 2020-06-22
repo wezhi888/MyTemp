1c048785980c1a5c5b401de9f3e7e5fdba7b71da
liuwenzhi <pwd@1nortH>
2020-06-22 13:32:23 +0800

    增加temp包和main方法运行的测试类

---
+++ src/main/java/top/lwzp/test/main/RunAsMain.java
@@ -0,0 +1,11 @@
+package top.lwzp.test.main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class RunAsMain {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println((System.currentTimeMillis() + ""));
+    }
+}
---
+++ src/main/java/top/lwzp/test/temp/TestInterface.java
@@ -0,0 +1,7 @@
+package top.lwzp.test.temp;
+
+@FunctionalInterface
+public interface TestInterface {
+
+    public int add(int a,int b);
+}
