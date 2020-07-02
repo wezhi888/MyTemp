1c9cdc596354f6a7d7bb55b5ed1d86aa35f10a95
liuwenzhi <pwd@1nortH>
2020-07-02 15:55:10 +0800

    update

--- src/main/java/top/lwzp/test/main/RunAsMain.java
+++ src/main/java/top/lwzp/test/main/RunAsMain.java
@@ -1,11 +1,33 @@
 package top.lwzp.test.main;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class RunAsMain {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println((System.currentTimeMillis() + ""));
+    public static void main(String[] args) {
+//        Stream stream = Stream.of("aa","ab","ac","bc");
+//        List<String> reduce = (List<String>) stream.reduce(new ArrayList<String>(), (a, b) -> {
+//            (a).add(b);
+//            return a;
+//        }, (a,b)->a);
+//        System.out.println(reduce);
     }
+}
+class Person{
+    private int age;
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
