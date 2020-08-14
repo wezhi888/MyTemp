60509b729097549e0a84e220799b4e930538c8c4
liuwenzhi <pwd@1nortH>
2020-08-14 15:17:27 +0800

    增加定时任务方法异步调用的注解@EnableAsync和@Async

--- src/main/java/top/lwzp/test/service/timer/TimerTask.java
+++ src/main/java/top/lwzp/test/service/timer/TimerTask.java
@@ -1,17 +1,20 @@
 package top.lwzp.test.service.timer;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-//@EnableScheduling
+@EnableScheduling
 public class TimerTask {
+    @Async
     @Scheduled(cron = "*/5 * * * * ?")
-    private void task(){
-        System.out.println(Thread.currentThread().getId() + " --- " + Thread.currentThread().getName());
-        System.out.println(System.currentTimeMillis());
+    public void task() throws InterruptedException {
+        System.out.println("time is :" + System.currentTimeMillis()/1000);
+        Thread.sleep(10000L);
     }
+
     @Scheduled(fixedDelay = 36000000)
     private void taskFixedDelay(){
         System.out.println("上一次执行完成之后，间隔若干时间再次执行！");
