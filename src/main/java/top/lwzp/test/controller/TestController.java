7cf87339e390f63e415e80756af57106f1937c29
liuwenzhi <pwd@1nortH>
2020-09-03 17:58:24 +0800

    RestTemplate post请求相关代码

--- src/main/java/top/lwzp/test/controller/TestController.java
+++ src/main/java/top/lwzp/test/controller/TestController.java
@@ -3,21 +3,26 @@ package top.lwzp.test.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import top.lwzp.test.controller.base.BaseController;
 import top.lwzp.test.vo.RestTemplateTestVo;
 
 import java.util.Date;
 
 @Controller
+@ResponseBody
 public class TestController extends BaseController {
-    @ResponseBody
-    @RequestMapping("/api/{name}/{age}")
-    public String api(@PathVariable("name") String name, @PathVariable("age")int age,String other) throws JsonProcessingException {
+    private ObjectMapper mapper = new ObjectMapper();
+    @RequestMapping(value = "/api/{name}/{age}",method = RequestMethod.GET)
+    public String apiGet(@PathVariable("name") String name, @PathVariable("age")int age,String other) throws JsonProcessingException {
         RestTemplateTestVo vo = new RestTemplateTestVo(age,name,new Date());
-        ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(vo);
     }
+
+    @PostMapping(value = "/post")
+    public String apiPost(String name,int age) throws JsonProcessingException {
+        RestTemplateTestVo vo = new RestTemplateTestVo(age,name,new Date());
+        return mapper.writeValueAsString(vo);
+    }
+
 }
