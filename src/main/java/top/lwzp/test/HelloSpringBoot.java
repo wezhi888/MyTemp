f77e4b240552aae47d9f3a5315c63df151f382df
liuwenzhi <pwd@1nortH>
2020-09-03 17:59:06 +0800

    update

--- src/main/java/top/lwzp/test/HelloSpringBoot.java
+++ src/main/java/top/lwzp/test/HelloSpringBoot.java
@@ -1,16 +1,23 @@
 package top.lwzp.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.*;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import top.lwzp.test.service.rest_template.RestTemplateService;
 import top.lwzp.test.service.spring.SpringUtil;
+import top.lwzp.test.vo.RestTemplateTestVo;
 
 import javax.swing.*;
 
@@ -18,13 +25,32 @@ import javax.swing.*;
 //@EnableScheduling
 //@EnableAsync
 public class HelloSpringBoot {
-
     public static void main(String[] args) {
+        //启动SpringBoot
         SpringApplication.run(HelloSpringBoot.class,args);
-        RestTemplateService re = SpringUtil.getBean(RestTemplateService.class);
-        String url = "";
-        String s = re.getForEntity(url);
-        System.out.println(s);
-        System.exit(-1);
+        try {
+            /*
+            获取RestTemplateService类中定义的restTemplate bean
+            如果RestTemplate没有交给Spring托管，也可以用new获取对象
+            */
+            RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
+            String url = "http://localhost:8080/post?pa=sss";
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+            params.add("name","王五");
+            params.add("age","21");
+            HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(params,headers);
+            ResponseEntity<String> entity = restTemplate.postForEntity(url,request,String.class,params);
+            HttpStatus statusCode = entity.getStatusCode();
+            System.out.println(entity.getBody());
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            System.exit(0);
+        }
+
+
     }
 }
--- src/main/java/top/lwzp/test/main/RunAsMain.java
+++ src/main/java/top/lwzp/test/main/RunAsMain.java
@@ -1,35 +1,11 @@
 package top.lwzp.test.main;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import top.lwzp.test.vo.JsonTestVo;
-
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class RunAsMain {
     public static void main(String[] args) throws IOException {
@@ -37,11 +13,9 @@ public class RunAsMain {
     }
 
     private static void testJackson() throws IOException {
-//        ObjectMapper mapper = new ObjectMapper();
-//        ArrayNode arrayNode = mapper.createArrayNode();
-//        ObjectNode objectNode = mapper.createObjectNode();
-        JsonNodeFactory factory = JsonNodeFactory.instance;
-        ArrayNode arrayNode = factory.arrayNode();
-        ObjectNode objectNode = factory.objectNode();
+        MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
+        map.add("name","张三");
+        map.add("name","李四");
+        System.out.println(map);
     }
 }
