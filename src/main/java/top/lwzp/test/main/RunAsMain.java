6d9ec41d0689cb76cd7b42cd44e7c0324be42e8b
liuwenzhi <pwd@1nortH>
2020-09-08 18:07:23 +0800

    增加apache工具类库的maven依赖

--- src/main/java/top/lwzp/test/main/RunAsMain.java
+++ src/main/java/top/lwzp/test/main/RunAsMain.java
@@ -1,21 +1,14 @@
 package top.lwzp.test.main;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import top.lwzp.test.vo.JsonTestVo;
-import java.io.IOException;
-import java.util.*;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 
 public class RunAsMain {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         testJackson();
     }
 
-    private static void testJackson() throws IOException {
-        MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
-        map.add("name","张三");
-        map.add("name","李四");
-        System.out.println(map);
+    private static void testJackson() {
+
     }
 }
