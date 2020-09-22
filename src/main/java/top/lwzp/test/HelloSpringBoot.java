4357b9e1d510c694224b98d29b374d633d611176
liuwenzhi <pwd@1nortH>
2020-09-22 17:45:41 +0800

    logback日志

--- src/main/java/top/lwzp/test/HelloSpringBoot.java
+++ src/main/java/top/lwzp/test/HelloSpringBoot.java
@@ -1,35 +1,19 @@
 package top.lwzp.test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.jasypt.encryption.StringEncryptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.*;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import top.lwzp.test.service.jdbc.JdbcTemplateService;
-import top.lwzp.test.service.rest_template.RestTemplateService;
-import top.lwzp.test.service.spring.SpringUtil;
-import top.lwzp.test.vo.RestTemplateTestVo;
+import top.lwzp.test.vo.JsonTestVo;
 
-import javax.swing.*;
-
-@SpringBootApplication
 //@EnableScheduling
 //@EnableAsync
+@SpringBootApplication
 public class HelloSpringBoot {
+    private static final Logger logger = LoggerFactory.getLogger(HelloSpringBoot.class);
     public static void main(String[] args) {
         //启动SpringBoot
         SpringApplication.run(HelloSpringBoot.class,args);
+        logger.info("logger{} debug{}","good","date");
     }
 }
